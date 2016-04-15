0d64fa6c3e729a4b3b668698f59bfa8386c707d8
Dallas_Carraher <dall133android@gmail.com>
2016-04-15 15:40:47 -0700

    card.h File
    header file for some classes (including card class)

---
+++ card.h
@@ -0,0 +1,34 @@
+//
+//  card.h
+//
+
+#ifndef card_h
+#define card_h
+
+
+
+
+
+class card{
+    
+private:
+    int card_value;
+    string suits[4] = { "hearts" , "clubs" , "spades" , "diamonds" };
+    string cardTypes[13] = { "Ace" , "2" , "3" , "4" , "5", "6" , "7" , "8" , "9" , "10" , "Jack" , "Queen" , "King" };
+    
+    
+public:
+    
+    
+    
+    
+};
+
+
+class
+
+
+
+
+
+#endif /* card_h */
